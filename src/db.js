7b366713bbb7156aa0f1395cb1b51691fe9af7e8
unknown <edgarrios412@gmail.com>
2023-11-24 11:22:38 -0500

    push

--- src/db.js
+++ src/db.js
@@ -4,12 +4,11 @@ const fs = require('fs');
 const path = require('path');
 const {DB} = process.env;
 
-const sequelize = new Sequelize(DB+"sslmode=require", {
+const sequelize = new Sequelize(DB+"?sslmode=require", {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
     dialect: 'postgres',
     protocol: 'postgres',
-    dialectOptions: {}, //removed ssl
   });
 
   const basename = path.basename(__filename);
