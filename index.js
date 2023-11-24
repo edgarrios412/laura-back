c24e8447612fea66a07afd50d70b70b7ed96289b
unknown <edgarrios412@gmail.com>
2023-11-24 11:34:44 -0500

    push

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {conn} = require("./src/db")
 
 conn.sync({alter:true}).then(() => {
     console.log("Conectado a la base de datos")
-    app.listen(3001, () => {
-        console.log("Servidor en linea en el puerto 3001")
+    app.listen(3002, () => {
+        console.log("Servidor en linea en el puerto 3002")
     })
 })
