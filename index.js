8812ac31e78b9c8a3697a98ea19041d4abebc4ba
unknown <edgarrios412@gmail.com>
2023-11-25 12:34:40 -0500

    push

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {conn} = require("./src/db")
 const {Procedimientos} = require("./src/db")
 const precios = require("./src/precios.js")
 
-conn.sync({force:true}).then(() => {
+conn.sync({alter:true}).then(() => {
     console.log("Conectado a la base de datos")
     Procedimientos.findAll()
     .then((data) => {
