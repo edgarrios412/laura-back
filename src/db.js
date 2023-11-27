e055125e455e56ecdcca27323baba75be074d88e
unknown <edgarrios412@gmail.com>
2023-11-27 11:26:34 -0500

    push

--- src/db.js
+++ src/db.js
@@ -53,5 +53,5 @@ Cotizacion.belongsTo(Client)
 
   module.exports = {
     ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-    conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+    conn: sequelize,     // para importart la conexión { conn } = require('./db.js')
   };
