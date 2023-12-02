b81a158297a7fde1d1a112fc4fb62596e7ae4d54
unknown <edgarrios412@gmail.com>
2023-12-02 11:53:23 -0500

    push

--- src/controllers/proveedorController.js
+++ src/controllers/proveedorController.js
@@ -21,5 +21,18 @@ module.exports = {
         }else{
             return "Proveedor no encontrado"
         }
+    },
+    putProveedor: async (data) => {
+        const proveedor = await Proveedor.findByPk(data.id)
+        if(proveedor){
+            if(data.name) proveedor.name = data.name
+            if(data.celular) proveedor.celular = data.celular
+            if(data.direccion) proveedor.direccion = data.direccion
+            if(data.departamento) proveedor.departamento = data.departamento
+            if(data.ciudad) proveedor.ciudad = data.ciudad
+            if(data.image) proveedor.image = data.image
+            await proveedor.save()
+            return "Proveedor editado con exito"
+        }else return "No encontrado"
     }
 }
--- src/routes/proveedorRoutes.js
+++ src/routes/proveedorRoutes.js
@@ -1,5 +1,5 @@
 const {Router} = require("express")
-const { getProveedor, getProveedores, postProveedor , deleteProveedor} = require("../controllers/proveedorController")
+const { getProveedor, getProveedores, postProveedor , deleteProveedor, putProveedor} = require("../controllers/proveedorController")
 const proveedorRoutes = Router()
 
 proveedorRoutes.get("/:id", async (req,res) => {
@@ -17,9 +17,10 @@ proveedorRoutes.post("/", async (req,res) => {
     return res.json(proveedor)
 })
 
-// proveedorRoutes.put("/", (req,res) => {
-//     res.json({users:"Edita un evolucion"})
-// })
+proveedorRoutes.put("/", async (req,res) => {
+    const edit = await putProveedor(req.body)
+    res.json(edit)
+})
 
 proveedorRoutes.delete("/:id", (req,res) => {
     const deleted = deleteProveedor(req.params.id)
