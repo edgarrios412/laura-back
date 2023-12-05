0035cd56c4155ae66d7ee3035e58ec6e23b4b221
unknown <edgarrios412@gmail.com>
2023-12-05 15:19:58 -0500

    push

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,7 @@ const temperaturaRoutes = require("./temperaturaRoutes")
 const evolucionRoutes = require("./evolucionRoutes")
 const proveedorRoutes = require("./proveedorRoutes")
 const inventarioRoutes = require("./inventarioRoutes")
+const recordatorioRoutes = require("./recordatorioRoutes")
 const indexRoutes = Router()
 
 indexRoutes.use("/user",userRoutes)
@@ -17,5 +18,6 @@ indexRoutes.use("/temperatura",temperaturaRoutes)
 indexRoutes.use("/evolucion",evolucionRoutes)
 indexRoutes.use("/proveedor",proveedorRoutes)
 indexRoutes.use("/inventario",inventarioRoutes)
+indexRoutes.use("/recordatorio",recordatorioRoutes)
 
 module.exports = indexRoutes
