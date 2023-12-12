92e31649d768b6f6596f152071e1f3a131a1f2df
unknown <edgarrios412@gmail.com>
2023-12-12 11:08:24 -0500

    push

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ const path = require('path');
 const {DB} = process.env;
 
 const sequelize = new Sequelize(DB+`?sslmode=require`, {
-    logging: false, // set to console.log to see the raw SQL queries
+    logging: console.log, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
     dialect: 'postgres',
     protocol: 'postgres',
